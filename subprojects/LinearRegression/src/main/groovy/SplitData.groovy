55b4998c4ca9a5c9ec8432eed61f1d3dda5f6cb5
Paul King <paulk@asert.com.au>
2019-06-19 12:11:30 +1000

    illustrate chop

--- subprojects/LinearRegression/src/main/groovy/SplitData.groovy
+++ subprojects/LinearRegression/src/main/groovy/SplitData.groovy
@@ -2,14 +2,17 @@
 def full = getClass().classLoader.getResource('kc_house_data.csv').file as File
 def parent = full.parentFile
 def lines = full.readLines()
-def splitAt = lines.size() * 0.8 as int
-
+//def splitAt = lines.size() * 0.8 as int
+def (trainLines, testLines) = lines.chop(lines.size() * 0.8 as int, -1)
 def train = new File(parent, 'house_train.csv')
-train.text = lines[0..<splitAt].join('\n')
+train.text = trainLines.join('\n')
+
+//train.text = lines[0..<splitAt].join('\n')
 
 def test = new File(parent, 'house_test.csv')
 test.delete()
-test << lines[0] << '\n' << lines[splitAt..-1].join('\n')
+//test << lines[0] << '\n' << lines[splitAt..-1].join('\n')
+test << lines[0] << '\n' << testLines.join('\n')
 
 println lines.size()
 println train.readLines().size()

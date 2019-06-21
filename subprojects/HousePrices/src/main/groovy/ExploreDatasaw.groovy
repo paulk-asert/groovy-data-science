35c0a79cb669c9f9907b5d269ae523564f9f8bec
Paul King <paulk@asert.com.au>
2019-06-21 15:06:02 +1000

    flesh out Datasaw example

--- subprojects/HousePrices/src/main/groovy/ExploreDatasaw.groovy
+++ subprojects/HousePrices/src/main/groovy/ExploreDatasaw.groovy
@@ -1,17 +1,18 @@
 //@Grab('tech.tablesaw:tablesaw-core:0.34.1')
+//@Grab('tech.tablesaw:tablesaw-aggregate:0.34.1')
 import tech.tablesaw.api.Table
+import static tech.tablesaw.aggregate.AggregateFunctions.*
 
 //def file = 'kc_house_data.csv' as File
-//def file = getClass().classLoader.getResource('kc_house_data.csv').file
-Table records = Table.read().csv("../resources/kc_house_data.csv")
+def file = getClass().classLoader.getResource('kc_house_data.csv').file
+Table records = Table.read().csv(file)
+//Table records = Table.read().csv("../resources/kc_house_data.csv")
 println records.shape()
-//records.findAll{ it.bedrooms.toInteger() > 10 }.each{ println it.toMap() as TreeMap }
 
-/*
-[bathrooms:3, bedrooms:11, condition:3, date:20140821T000000, floors:2, grade:7, id:1773100755, lat:47.556,
- long:-122.363, price:520000, sqft_above:2400, sqft_basement:600, sqft_living:3000, sqft_living15:1420,
- sqft_lot:4960, sqft_lot15:4960, view:0, waterfront:0, yr_built:1918, yr_renovated:1999, zipcode:98106]
-[bathrooms:1.75, bedrooms:33, condition:5, date:20140625T000000, floors:1, grade:7, id:2402100895, lat:47.6878,
- long:-122.331, price:640000, sqft_above:1040, sqft_basement:580, sqft_living:1620, sqft_living15:1330,
- sqft_lot:6000, sqft_lot15:4700, view:0, waterfront:0, yr_built:1947, yr_renovated:0, zipcode:98103]
- */
+println records.structure()
+println records.where(records.column("bedrooms").isGreaterThan(10))
+
+def cleaned = records.dropWhere(records.column("bedrooms").isGreaterThan(30))
+
+println cleaned.shape()
+println records.summarize("price", mean, min, max).by("bedrooms")

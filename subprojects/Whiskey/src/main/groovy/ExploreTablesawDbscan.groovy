e07c01360348e5d0e8a073c6aad3a8d3a1b35652
Paul King <paulk@asert.com.au>
2019-07-05 00:31:17 +1000

    add XMeans example

---
+++ subprojects/Whiskey/src/main/groovy/ExploreTablesawDbscan.groovy
@@ -0,0 +1,35 @@
+import smile.clustering.XMeans
+import smile.projection.PCA
+import tech.tablesaw.api.*
+import tech.tablesaw.plotly.Plot
+import tech.tablesaw.plotly.api.*
+
+// source: https://www.niss.org/sites/default/files/ScotchWhisky01.txt
+def file = getClass().classLoader.getResource('whiskey.csv').file
+def rows = Table.read().csv(file)
+//Table rows = Table.read().csv('whiskey.csv')
+
+def cols = ["Body", "Sweetness", "Smoky", "Medicinal",
+            "Tobacco", "Honey", "Spicy", "Winey",
+            "Nutty", "Malty", "Fruity", "Floral"]
+def data = rows.as().doubleMatrix(*cols)
+
+def pca = new PCA(data)
+def dims = 4 // can be 2, 3 or 4
+pca.projection = dims
+def projected = pca.project(data)
+def adj = [1, 1, 1, 5]
+def kmax = 10
+def groups = new XMeans(data, kmax)
+rows = rows.addColumns(
+    *(0..<dims).collect { idx ->
+        DoubleColumn.create("PCA${idx+1}", (0..<data.size()).collect{
+            adj[idx] * (projected[it][idx] + adj[idx])
+        })
+    },
+    StringColumn.create("Cluster", groups.clusterLabel.collect{ "Cluster" + (it+1) })
+)
+
+def title = "Clusters x Principal Components"
+def type = dims == 2 ? ScatterPlot : Scatter3DPlot
+Plot.show(type.create(title, rows, *(1..dims).collect { "PCA$it" }, "Cluster"))

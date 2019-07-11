3f291ada33c73df3dbb73ed40d9a5058d0594b34
Paul King <paulk@asert.com.au>
2019-07-11 18:26:42 +1000

    tweaks

--- subprojects/WhiskeySpark/src/main/groovy/WhiskeySpark.groovy
+++ subprojects/WhiskeySpark/src/main/groovy/WhiskeySpark.groovy
@@ -12,9 +12,8 @@ def file = WhiskeySpark.classLoader.getResource('whiskey.csv').file
 int k = 5
 Dataset<Row> rows = spark.read().format('com.databricks.spark.csv')
         .options('header': 'true', 'inferSchema': 'true').load(file)
-def extras = ['RowID', 'Distillery']
 //def colNames = rows.columns().toList().minus(extras).parallelStream().toArray(String[]::new)
-String[] colNames = rows.columns().toList().minus(extras)
+String[] colNames = rows.columns().toList().minus(['RowID', 'Distillery'])
 def assembler = new VectorAssembler(inputCols: colNames, outputCol: 'features')
 Dataset<Row> dataset = assembler.transform(rows)
 def clusterer = new KMeans(k: k, seed: 1L)

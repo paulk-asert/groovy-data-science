49342d98ff3d787ace18f91c35c712761ffa6058
Paul King <paulk@asert.com.au>
2019-07-13 00:57:27 +1000

    prepare for HousePricesBeam example

--- subprojects/HousePricesBeam/src/main/groovy/LinearRegressionMultiGPars.groovy
+++ subprojects/HousePricesBeam/src/main/groovy/LinearRegressionMultiGPars.groovy
@@ -1,10 +1,14 @@
-//import StatUtils
+import org.apache.commons.math3.stat.StatUtils
+import smile.math.Math
+import smile.math.distance.EuclideanDistance
 import smile.regression.OLS
 import tech.tablesaw.api.*
 import groovyx.gpars.GParsPool
 
+import static java.lang.Math.sqrt
+
 def features = [
-        'bedrooms', 'bathrooms', 'sqft_living', 'sqft_living15', 'lat',
+        'price', 'bedrooms', 'bathrooms', 'sqft_living', 'sqft_living15', 'lat',
         'sqft_above', 'grade', 'view', 'waterfront', 'floors'
 ]
 
@@ -12,25 +16,41 @@ def idxs = 0..<features.size()
 def file = getClass().classLoader.getResource('kc_house_data.csv').file
 def table = Table.read().csv(file)
 table = table.dropWhere(table.column("bedrooms").isGreaterThan(30))
-
 def data = table.as().doubleMatrix(*features)
-//def dataT = data.transpose()
-//def price = dataT[priceIdx]
-def model = new OLS(data, table.column('price').collect{ it } as double[])
-println "Coefficients: ${model.coefficients()}"
 
 GParsPool.withPool {
-    def numbers = 1..4
-    def maxPromise = numbers.inject(0, {a, b -> a>b?a:b}.asyncFun())
+    def trainChunkSize = 5000
+    def numTrainChunks = 5
+    def models = (0..<numTrainChunks).collectParallel {
+        def list = data.toList()
+        Collections.shuffle(list)
+        list = list.take(trainChunkSize)
+        new OLS(list.collect{ it[1..-1] } as double[][], list.collect{ it[0] } as double[]).with{ [it.intercept(), *it.coefficients()] }
+    }
+    models.each {
+        println "Intercept: ${it[0]}, Coefficients: ${it[1..-1]}"
+    }
+    def model = models.transpose()*.sum().collect{ it/numTrainChunks }
+    println "Averaged =>\nIntercept: ${model[0]}, Coefficients: ${model[1..-1]}"
 
-    println "Look Ma, I can talk to the user while the math is being done for me!"
-    println maxPromise.get()
+    double[] coefficients = model[1..-1]
+    double intercept = model[0]
+    double[] predicted = data.collect { row -> intercept + Math.dot(row[1..-1] as double[], coefficients) }
+    double[] residuals = data.toList().indexed().collect { idx, row -> predicted[idx] - row[0] }
+    def rmse = sqrt(StatUtils.sumSq(residuals as double[]) / data.size())
+    def mean = Math.mean(residuals as double[])
+    println "$rmse $mean"
+    def evalChunkSize = 2000
+    def results = data.collate(evalChunkSize).collectParallel { chunk ->
+        predicted = chunk.collect { row -> intercept + Math.dot(row[1..-1] as double[], coefficients) }
+        residuals = chunk.indexed().collect { idx, row -> predicted[idx] - row[0] }
+        rmse = sqrt(StatUtils.sumSq(residuals as double[]) / chunk.size())
+        mean = Math.mean(residuals as double[])
+        [rmse, mean, chunk.size()]
+    }
+    results.each {
+        println it
+    }
+    def resultsT = results.transpose()
+    println sqrt(resultsT.collect{ (it[0] * it[0] * it[2]) }.sum() / data.size())
 }
-
-//def predicted = test.collect { data -> params[0] + (1..<params.size()).collect{ data[it-1] * params[it] }.sum() }
-//def residuals = test.indexed().collect { i, data -> predicted[i] - data[priceIdx] }
-//def rr = reg.calculateRSquared()
-//def rmseTrain = Math.sqrt(reg.calculateResidualSumOfSquares() / (train.size() - 1))
-//def rmseTest = Math.sqrt(StatUtils.sumSq(residuals as double[]) / (test.size() - 1))
-//def mean = StatUtils.mean(residuals as double[])
-//println "$rr $rmseTrain $rmseTest $mean"

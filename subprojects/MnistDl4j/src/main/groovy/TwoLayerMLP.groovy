344a3cc98c4de8dd6028b1b3003ec8bbc5b19f27
Paul King <paulk@asert.com.au>
2019-07-29 15:11:10 +1000

    DL4J intro example

--- subprojects/MnistDl4j/src/main/groovy/TwoLayerMLP.groovy
+++ subprojects/MnistDl4j/src/main/groovy/TwoLayerMLP.groovy
@@ -3,47 +3,48 @@ import org.deeplearning4j.nn.conf.NeuralNetConfiguration
 import org.deeplearning4j.nn.conf.layers.DenseLayer
 import org.deeplearning4j.nn.conf.layers.OutputLayer
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
-import org.deeplearning4j.nn.weights.WeightInit
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener
 import org.nd4j.linalg.activations.Activation
-import org.nd4j.linalg.learning.config.Nesterovs
+import org.nd4j.linalg.learning.config.Nadam
 import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction
 import org.slf4j.LoggerFactory
 
-/** A Multi Layered Perceptron (MLP) with a single hidden layer,
+/*
+ * A Multi Layered Perceptron (MLP) with two input layers and one hidden layer,
  * applied to the digit classification task of the MNIST Dataset
  *
  * Based on the corresponding example from DL4J:
- * github.com/deeplearning4j/dl4j-examples/blob/master/dl4j-examples/src/main/java/org/deeplearning4j/examples/feedforward/mnist/MLPMnistSingleLayerExample.java
+ * github.com/deeplearning4j/dl4j-examples/blob/master/dl4j-examples/src/main/java/org/deeplearning4j/examples/feedforward/mnist/MLPMnistTwoLayerExample.java
  */
 int rngSeed = 123
-int batchSize = 125
+int batchSize = 64
 int numEpochs = 15
 int numInputs = 28 * 28
-int hiddenLayerSize = 1000
 int numOutputs = 10
+double rate = 0.0015
 
 def (trainSet, testSet) = [true, false].collect { new MnistDataSetIterator(batchSize, it, rngSeed) }
 def log = LoggerFactory.getLogger(getClass())
 def conf = new NeuralNetConfiguration.Builder()
         .seed(rngSeed) //include the random seed for reproducibility
         // use stochastic gradient descent as an optimization algorithm
-        .updater(new Nesterovs(0.006, 0.9))
-        .l2(1e-4)
+        .updater(new Nadam())
+        .l2(rate * 0.005) // regularized
         .list()
-        // hidden layer:
-        .layer(0, new DenseLayer.Builder()
+        // first input layer:
+        .layer(new DenseLayer.Builder()
                 .nIn(numInputs)
-                .nOut(hiddenLayerSize)
-                .activation(Activation.RELU)
-                .weightInit(WeightInit.XAVIER)
+                .nOut(500)
                 .build())
-        // output layer:
-        .layer(1, new OutputLayer.Builder(LossFunction.NEGATIVELOGLIKELIHOOD)
-                .nIn(hiddenLayerSize)
+        // second input layer:
+        .layer(new DenseLayer.Builder()
+                .nIn(500)
+                .nOut(100)
+                .build())
+        // hidden layer:
+        .layer(new OutputLayer.Builder(LossFunction.NEGATIVELOGLIKELIHOOD)
                 .nOut(numOutputs)
                 .activation(Activation.SOFTMAX)
-                .weightInit(WeightInit.XAVIER)
                 .build())
         .build()
 

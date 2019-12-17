b7f308d2851646fa5e4ff6cd4796ffa071d26ccd
Paul King <paulk@asert.com.au>
2019-12-17 17:46:45 +1000

    simplify charts

--- subprojects/HousePrices/src/main/groovy/MultiVariance.groovy
+++ subprojects/HousePrices/src/main/groovy/MultiVariance.groovy
@@ -1,10 +1,11 @@
 import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression
 import org.knowm.xchart.SwingWrapper
 import org.knowm.xchart.XYChartBuilder
-import org.knowm.xchart.XYSeries
-import org.knowm.xchart.style.markers.SeriesMarkers
 
 import static org.apache.commons.csv.CSVFormat.RFC4180 as CSV
+import static org.knowm.xchart.XYSeries.XYSeriesRenderStyle.Line
+import static org.knowm.xchart.XYSeries.XYSeriesRenderStyle.Scatter
+import static org.knowm.xchart.style.markers.SeriesMarkers.NONE
 
 def file = getClass().classLoader.getResource('kc_house_data.csv').file
 def csv  = CSV.withFirstRecordAsHeader().parse(new FileReader(file))
@@ -17,16 +18,13 @@ def betas = reg.estimateRegressionParameters()
 def predicted = features.collect{ row -> row.indices.collect{ i -> betas[i+1] * row[i] }.sum() + betas[0] }
 
 def chart = new XYChartBuilder().width(900).height(450).title("Actual vs predicted price").xAxisTitle("Actual").yAxisTitle("Predicted").build()
-chart.addSeries("Price", price as double[], predicted as double[])
-chart.styler.with {
-    XAxisLabelRotation = 90
-    availableSpaceFill = 0.98
-    defaultSeriesRenderStyle = XYSeries.XYSeriesRenderStyle.Scatter
+chart.addSeries("Price", price as double[], predicted as double[]).with {
+    XYSeriesRenderStyle = Scatter
 }
 def from = [price.min(), predicted.min()].min()
 def to = [price.max(), predicted.max()].min()
 chart.addSeries("exact", [from, to] as double[], [from, to] as double[]).with {
-    marker = SeriesMarkers.NONE
-    XYSeriesRenderStyle = XYSeries.XYSeriesRenderStyle.Line
+    marker = NONE
+    XYSeriesRenderStyle = Line
 }
 new SwingWrapper(chart).displayChart()

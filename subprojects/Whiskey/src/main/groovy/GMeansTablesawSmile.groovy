bdada5531565c874c519544085fdcd1a0d027ed4
Paul King <paulk@asert.com.au>
2019-12-30 21:34:43 +1000

    add GMeans example

---
+++ subprojects/Whiskey/src/main/groovy/GMeansTablesawSmile.groovy
@@ -0,0 +1,34 @@
+import smile.clustering.GMeans
+import smile.projection.PCA
+import tech.tablesaw.api.*
+import tech.tablesaw.plotly.Plot
+import tech.tablesaw.plotly.api.*
+
+def file = getClass().classLoader.getResource('whiskey.csv').file
+def rows = Table.read().csv(file)
+//Table rows = Table.read().csv('whiskey.csv')
+
+def cols = ["Body", "Sweetness", "Smoky", "Medicinal", "Tobacco", "Honey",
+            "Spicy", "Winey", "Nutty", "Malty", "Fruity", "Floral"]
+def data = rows.as().doubleMatrix(*cols)
+
+def pca = new PCA(data)
+def dims = 4 // can be 2, 3 or 4
+pca.projection = dims
+def projected = pca.project(data)
+def adj = [1, 1, 1, 5]
+def kmax = 10
+def clusterer = new GMeans(data, kmax)
+def labels = clusterer.clusterLabel.collect { "Cluster " + (it + 1) }
+rows = rows.addColumns(
+    *(0..<dims).collect { idx ->
+        DoubleColumn.create("PCA${idx+1}", (0..<data.size()).collect{
+            adj[idx] * (projected[it][idx] + adj[idx])
+        })
+    },
+    StringColumn.create("Cluster", labels)
+)
+
+def title = "Clusters x Principal Components"
+def type = dims == 2 ? ScatterPlot : Scatter3DPlot
+Plot.show(type.create(title, rows, *(1..dims).collect { "PCA$it" }, "Cluster"))

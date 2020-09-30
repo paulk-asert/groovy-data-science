4953bfebb20abdf779636d2da4bcf3cfc6e02f8c
Paul King <paulk@asert.com.au>
2020-10-01 01:02:57 +1000

    additional minor tweaks

--- subprojects/HousePricesGPars/src/main/groovy/LinearRegressionMultiGPars.groovy
+++ subprojects/HousePricesGPars/src/main/groovy/LinearRegressionMultiGPars.groovy
@@ -1,10 +1,10 @@
-import org.apache.commons.math3.stat.StatUtils
-import smile.math.Math
 import smile.regression.OLS
 import tech.tablesaw.api.*
 import groovyx.gpars.GParsPool
 
 import static java.lang.Math.sqrt
+import static org.apache.commons.math3.stat.StatUtils.sumSq
+import static smile.math.Math.dot
 
 def features = [
         'price', 'bedrooms', 'bathrooms', 'sqft_living', 'sqft_living15', 'lat',
@@ -20,9 +20,7 @@ GParsPool.withPool {
     def trainChunkSize = 3000
     def numTrainChunks = 8
     def models = (0..<numTrainChunks).collectParallel {
-        def list = data.toList()
-        Collections.shuffle(list)
-        list = list.take(trainChunkSize)
+        def list = data.toList().shuffled().take(trainChunkSize)
         new OLS(list.collect{ it[1..-1] } as double[][], list.collect{ it[0] } as double[]).with{ [it.intercept(), *it.coefficients()] }
     }
 //    models.each {
@@ -35,11 +33,10 @@ GParsPool.withPool {
     double[] coefficients = model[1..-1]
     double intercept = model[0]
     def stats = { chunk ->
-        def predicted = chunk.collect { row -> intercept + Math.dot(row[1..-1] as double[], coefficients) }
+        def predicted = chunk.collect { row -> intercept + dot(row[1..-1] as double[], coefficients) }
         def residuals = chunk.toList().indexed().collect { idx, row -> predicted[idx] - row[0] }
-        def rmse = sqrt(StatUtils.sumSq(residuals as double[]) / chunk.size())
-        def mean = Math.mean(residuals as double[])
-        [rmse, mean, chunk.size()]
+        def rmse = sqrt(sumSq(residuals as double[]) / chunk.size())
+        [rmse, residuals.average(), chunk.size()]
     }
 //    println stats(data)
     def evalChunkSize = 2000

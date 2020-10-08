67c04747024fb8f9c3bd4a75474f93013297452c
Paul King <paulk@asert.com.au>
2020-10-08 10:43:53 +1000

    simplify by using ELKIBuilder more

--- subprojects/Whiskey/src/main/groovy/KMeans_Elki.groovy
+++ subprojects/Whiskey/src/main/groovy/KMeans_Elki.groovy
@@ -1,38 +1,28 @@
 //@Grab('de.lmu.ifi.dbs.elki:elki:0.7.5')
 import de.lmu.ifi.dbs.elki.algorithm.clustering.kmeans.KMeansLloyd
-import de.lmu.ifi.dbs.elki.algorithm.clustering.kmeans.initialization.KMeansPlusPlusInitialMeans
-import de.lmu.ifi.dbs.elki.data.DoubleVector
-import de.lmu.ifi.dbs.elki.data.NumberVector
 import de.lmu.ifi.dbs.elki.database.StaticArrayDatabase
 import de.lmu.ifi.dbs.elki.database.ids.DBIDIter
 import de.lmu.ifi.dbs.elki.datasource.FileBasedDatabaseConnection
 import de.lmu.ifi.dbs.elki.datasource.parser.NumberVectorLabelParser
-import de.lmu.ifi.dbs.elki.distance.distancefunction.minkowski.EuclideanDistanceFunction
 import de.lmu.ifi.dbs.elki.result.AutomaticVisualization
 import de.lmu.ifi.dbs.elki.utilities.ELKIBuilder
-import de.lmu.ifi.dbs.elki.utilities.random.RandomFactory
 //import de.lmu.ifi.dbs.elki.visualization.VisualizerParameterizer
-
-import static de.lmu.ifi.dbs.elki.datasource.parser.CSVReaderFormat.DEFAULT_FORMAT
 
 def cols = ['Body', 'Sweetness', 'Smoky', 'Medicinal', 'Tobacco', 'Honey',
             'Spicy', 'Winey', 'Nutty', 'Malty', 'Fruity', 'Floral']
 
 def file = getClass().classLoader.getResource('whiskey.csv').file
-def parser = new NumberVectorLabelParser(DEFAULT_FORMAT, [3] as long[], DoubleVector.FACTORY)
+def parser = new ELKIBuilder(NumberVectorLabelParser).with('parser.labelIndices', '0,1').build()
 def dbc = new FileBasedDatabaseConnection([], parser, file)
 def db = new StaticArrayDatabase(dbc, null)
 db.initialize()
 
-def dist = EuclideanDistanceFunction.STATIC
-def init = new KMeansPlusPlusInitialMeans(RandomFactory.DEFAULT)
-
-def kmeans = new KMeansLloyd<NumberVector>(dist, 4, 0, init)
+def kmeans = new ELKIBuilder(KMeansLloyd).with('kmeans.k', 4).build()
 def c = kmeans.run(db)
 
 def auto = new ELKIBuilder(AutomaticVisualization)
         .with(AutomaticVisualization.Parameterizer.WINDOW_TITLE_ID, 'Whiskey clusters')
-//        .with(VisualizerParameterizer.Parameterizer.ENABLEVIS_ID, 'scatter|parallel|key')
+        //.with(VisualizerParameterizer.Parameterizer.ENABLEVIS_ID, 'scatter|parallel|key')
         .build()
 
 def hier = db.hierarchy

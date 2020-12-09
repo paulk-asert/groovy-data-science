f60223738bc9e3b6b0ddb3573821dd4b06fefe48
Paul King <paulk@asert.com.au>
2020-12-09 21:52:59 +1000

    add license headers

--- subprojects/HousePricesSpark/src/main/groovy/HousePricesSpark.groovy
+++ subprojects/HousePricesSpark/src/main/groovy/HousePricesSpark.groovy
@@ -1,3 +1,18 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 //@Grab("com.fasterxml.jackson.module:jackson-module-scala_2.11:2.11.1;transitive=false")
 //@Grab('org.apache.spark:spark-sql_2.11:2.4.7')
 //@Grab('org.apache.spark:spark-mllib_2.11:2.4.7')

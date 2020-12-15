dc1eb1e81abfa4b28cda0a2bf519f1eca72920bd
Paul King <paulk@asert.com.au>
2020-12-15 18:17:04 +1000

    adjust for latest Smile version

---
+++ subprojects/Whiskey/src/main/groovy/KMeansPcaScree_Smile.groovy
@@ -0,0 +1,44 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import smile.clustering.KMeans
+import smile.io.Read
+import smile.plot.swing.PlotGrid
+import smile.plot.swing.ScatterPlot
+import smile.plot.swing.ScreePlot
+import smile.projection.PCA
+
+import static org.apache.commons.csv.CSVFormat.RFC4180 as CSV
+
+def file = new File(getClass().classLoader.getResource('whiskey.csv').file)
+def table = Read.csv(file.toPath(), CSV.withFirstRecordAsHeader())
+
+String[] cols = ["Body", "Sweetness", "Smoky", "Medicinal", "Tobacco", "Honey",
+                 "Spicy", "Winey", "Nutty", "Malty", "Fruity", "Floral"]
+def data = table.select(cols).toArray()
+
+def pca = PCA.fit(data)
+pca.projection = 2
+def plots = [new ScreePlot(pca)]
+def projected = pca.project(data)
+char mark = '#'
+
+(2..6).each { k ->
+    println "Processing cluster size $k"
+    def clusters = KMeans.fit(data, k)
+    plots << ScatterPlot.of(projected, clusters.y, mark)
+}
+
+new PlotGrid(*plots*.canvas()*.panel()).window()

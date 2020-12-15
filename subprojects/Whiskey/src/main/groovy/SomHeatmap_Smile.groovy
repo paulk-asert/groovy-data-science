b47757eebcb5413cb50df953ea1bdc3efa0a3c8d
Paul King <paulk@asert.com.au>
2020-12-16 00:27:45 +1000

    adjust for latest Smile version

---
+++ subprojects/Whiskey/src/main/groovy/SomHeatmap_Smile.groovy
@@ -0,0 +1,56 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import smile.io.Read
+import smile.math.MathEx
+import smile.math.TimeFunction
+import smile.plot.swing.*
+import smile.vq.Neighborhood
+import smile.vq.SOM
+
+import static org.apache.commons.csv.CSVFormat.RFC4180 as CSV
+
+def file = new File(getClass().classLoader.getResource('whiskey.csv').file)
+def table = Read.csv(file.toPath(), CSV.withFirstRecordAsHeader())
+
+String[] cols = ['Body', 'Sweetness', 'Smoky', 'Medicinal', 'Tobacco', 'Honey',
+                 'Spicy', 'Winey', 'Nutty', 'Malty', 'Fruity', 'Floral']
+def data = table.select(cols).toArray()
+def distilleries = table.column('Distillery').toStringArray()
+
+//def lattice = SOM.lattice(3, 3, data)
+//def lattice = SOM.lattice(5, 4, data)
+def lattice = SOM.lattice(7, 6, data)
+//def lattice = SOM.lattice(8, 6, data)
+
+int epochs = 100
+def model = new SOM(lattice,
+        TimeFunction.constant(0.1),
+        Neighborhood.Gaussian(1, data.length * epochs / 8.0))
+for (int i = 0; i < epochs; i++) {
+    for (int j : MathEx.permutate(data.length)) {
+        model.update(data[j])
+    }
+}
+
+def groups = data.toList().indices.groupBy { idx ->
+    model.quantize(data[idx]).collect{sprintf('%.5f', it) }.join(' ')
+}
+groups.eachWithIndex { k, v, i ->
+    println "Cluster $i: ${v.collect{ distilleries[it] }.join(', ')}"
+}
+
+def hexmap = Hexmap.of(model.umatrix(), Palette.jet(256))
+hexmap.canvas().tap{setAxisLabels('', '') }.window()

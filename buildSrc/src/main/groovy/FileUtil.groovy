47c747b75fd4e7777e74356b712055384d7914f1
Paul King <paulk@asert.com.au>
2021-07-03 00:59:55 +1000

    better warnings when using different JDKs

--- buildSrc/src/main/groovy/FileUtil.groovy
+++ buildSrc/src/main/groovy/FileUtil.groovy
@@ -2,7 +2,11 @@ class FileUtil {
     private FileUtil() {}
 
     static List<String> baseNames(File subdir, List<String> exclusions = [], String ext = '.groovy') {
-        subdir.listFiles()*.name
+        baseNames(subdir.listFiles().toList(), exclusions, ext)
+    }
+
+    static List<String> baseNames(Collection<File> files, List<String> exclusions = [], String ext = '.groovy') {
+        files*.name
                 .findAll { it.endsWith(ext) }
                 .collect { it - ext }
                 .findAll { !(it in exclusions) && !it.endsWith('Util') }

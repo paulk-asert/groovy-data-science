8ccc638d6d69cd1e705e35440567e538b3edc8c5
Paul King <paulk@asert.com.au>
2021-07-17 13:08:44 +1000

    minor refactor

--- subprojects/HousePrices/src/main/groovy/FindOutlierSVM_Tribuo.groovy
+++ subprojects/HousePrices/src/main/groovy/FindOutlierSVM_Tribuo.groovy
@@ -19,7 +19,6 @@ import org.tribuo.anomaly.AnomalyFactory
 import org.tribuo.anomaly.evaluation.AnomalyEvaluator
 import org.tribuo.anomaly.libsvm.LibSVMAnomalyTrainer
 import org.tribuo.anomaly.libsvm.SVMAnomalyType
-import org.tribuo.classification.Label
 import org.tribuo.common.libsvm.SVMParameters
 import org.tribuo.data.columnar.RowProcessor
 import org.tribuo.data.columnar.processors.field.DoubleFieldProcessor
@@ -32,27 +31,29 @@ import static org.tribuo.anomaly.libsvm.SVMAnomalyType.SVMMode.ONE_CLASS
 import static org.tribuo.common.libsvm.KernelType.RBF
 
 def uri = getClass().classLoader.getResource('kc_house_data.csv').toURI()
-def fieldProcessors = [
-        bedrooms : new DoubleFieldProcessor('bedrooms'),
-        bathrooms: new DoubleFieldProcessor('bathrooms'),
-]
-def rowProcessor = new RowProcessor<Label>(new FieldResponseProcessor('price', '0', new AnomalyFactory()), fieldProcessors)
-def dataSource = new CSVDataSource<Label>(uri, rowProcessor, true)
+def fieldProcessors = ['bedrooms', 'bathrooms', 'grade'].collectEntries { [(it): new DoubleFieldProcessor(it)] }
+def responseProcessor = new FieldResponseProcessor('price', '0', new AnomalyFactory())
+def rowProcessor = new RowProcessor(responseProcessor, fieldProcessors)
+def dataSource = new CSVDataSource(uri, rowProcessor, true)
 def data = new MutableDataset(dataSource)
 def eval = new AnomalyEvaluator()
 
-def params = new SVMParameters<>(new SVMAnomalyType(ONE_CLASS), RBF)
+def params = new SVMParameters(new SVMAnomalyType(ONE_CLASS), RBF)
 params.gamma = 0.04
-params.nu = 0.0008
+params.nu = 0.00075
 def trainer = new LibSVMAnomalyTrainer(params)
 
 def model = trainer.train(data)
 println "\nFinding anomalies with: ${formattedProvenanceString(model.provenance.trainerProvenance)}"
 
 def predictions = model.predict(data)
-def anomalies = predictions.indexed().collectMany{ i, prediction ->
-    prediction.output.type == ANOMALOUS ? [data.getExample(i).toList()] : []
+def results = predictions.indexed().collect{ i, prediction ->
+    [prediction.output, data.getExample(i)]
 }
+def anomalies = results
+        .findAll{it[0].type == ANOMALOUS }
+        .sort{ it[0].score }
+        .collect{it[1].toList() }
 if (anomalies) {
     println "\nPotential anomalies:\n${anomalies.join('\n')}"
 }

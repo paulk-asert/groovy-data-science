d4e3ea647b2eeb6c41f649d18c11eb16620bebac
paulk-asert <paulk@asert.com.au>
2022-06-08 15:14:03 +1000

    use djl above MXNet

--- subprojects/DeepLearningMxnet/src/main/groovy/ObjectDetect.groovy
+++ subprojects/DeepLearningMxnet/src/main/groovy/ObjectDetect.groovy
@@ -13,76 +13,63 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// Unfortunately the MXNet Java Inference API is an extension of the
-// Scala Infer API which isn't yet available on Windows.
-// You'll see an error including: mxnet-scala.dll not found under Windows.
-// For now, use another OS (in a VM if needed).
-//@GrabExclude("commons-codec#commons-codec:1.10")
-//@Grab("org.apache.mxnet:mxnet-full_2.11-linux-x86_64-cpu:1.5.1")
-//@Grab("org.slf4j:slf4j-api:1.7.31")
-//@Grab("org.slf4j:slf4j-jdk14:1.7.31")
+import ai.djl.Application
+import ai.djl.engine.Engine
+import ai.djl.modality.cv.output.DetectedObjects
+import ai.djl.repository.zoo.Criteria
+import ai.djl.training.util.ProgressBar
 import groovy.swing.SwingBuilder
 import javax.imageio.ImageIO
-import org.apache.mxnet.infer.javaapi.ObjectDetector
-import org.apache.mxnet.javaapi.*
+import ai.djl.modality.cv.*
+
+import java.nio.file.Files
+import java.nio.file.Path
+
 import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE
 
-static void downloadUrl(String srcDirUrl, String destDir, String filename) {
-    def destination = new File(destDir, filename)
-    if (!destination.parentFile.exists()) {
-        destination.parentFile.mkdirs()
-    }
-    if (!destination.exists()) {
-        destination.bytes = new URL(srcDirUrl + filename).bytes
-    }
-}
+Path baseDir = Files.createTempDirectory("resnetssd")
+def imageName = 'dog-ssd.jpg'
+String imgURL = "https://s3.amazonaws.com/model-server/inputs/"
+println "Downloading image to ${baseDir}..."
+def imagePath = baseDir.resolve(imageName)
+def imageSaved = baseDir.resolve('detected.png')
+imagePath.bytes = new URL(imgURL + imageName).bytes
 
-static downloadModelImage() {
-    String baseDir = "${System.getProperty('java.io.tmpdir')}/resnetssd/"
-    def imageName = 'dog-ssd.jpg'
-    def modelName = 'resnet50_ssd_model'
-    String imgURL = "https://s3.amazonaws.com/model-server/inputs/"
-    println "Downloading image to ${baseDir}..."
-    downloadUrl(imgURL, baseDir, imageName)
-    println "Downloading model files to ${baseDir}... (may take a while)"
-    String modelURL = "https://s3.amazonaws.com/model-server/models/resnet50_ssd/"
-    downloadUrl(modelURL, baseDir, "$modelName-symbol.json")
-    downloadUrl(modelURL, baseDir, "$modelName-0000.params")
-    downloadUrl(modelURL, baseDir, 'synset.txt')
-    [baseDir + imageName, baseDir + modelName]
-}
+Image img = ImageFactory.instance.fromFile(imagePath)
 
-static detectObjects(String modelPath, String imagePath, inputShape) {
-    def context = [Context.cpu()]
-    def inputDescriptor = new DataDesc("data", inputShape, DType.Float32(), "NCHW")
-    def objDet = new ObjectDetector(modelPath, [inputDescriptor], context, 0)
-    return objDet.imageObjectDetect(ObjectDetector.loadImageFromFile(imagePath), 3)
-}
+def criteria =
+        Criteria.builder()
+                .optApplication(Application.CV.OBJECT_DETECTION)
+                .setTypes(Image, DetectedObjects)
+                .optFilter("backbone", "resnet50")
+                .optEngine(Engine.defaultEngineName)
+                .optProgress(new ProgressBar())
+                .build()
 
-def (imagePath, modelPath) = downloadModelImage()
-def image = ImageIO.read(imagePath as File)
+def image = ImageIO.read(imagePath.toFile())
 def (w, h) = image.with{ [it.width, it.height] }
-def count = 1 // batch size
-def channels = 3 // for RGB
-def inputShape = new Shape(count, channels, w * 0.8 as int, h)
-def results = detectObjects(modelPath, imagePath, inputShape).sum()
+def resultCount
 
-def boxes = results.collect {[
-        xmin: w * it.XMin as int, ymin: h * it.YMin as int,
-        xmax: w * it.XMax as int, ymax: h * it.YMax as int
-]}
-def names = results.collect{ it.className + sprintf(' %.3f', it.probability) }
-(0..<names.size()).each{ println "${names[it]} ${boxes[it]}" }
-
-Image.drawBoundingBox(image, boxes, names)
-new SwingBuilder().edt {
-    frame(title: "${results.size()} detected objects", size: [w, h],
-            show: true, defaultCloseOperation: DISPOSE_ON_CLOSE) {
-        label(icon: imageIcon(image: image))
+criteria.loadModel().withCloseable { model ->
+    model.newPredictor().withCloseable { predictor ->
+        def detection = predictor.predict(img)
+        resultCount = detection.numberOfObjects
+        detection.items().each{ println it }
+        img.drawBoundingBoxes(detection)
+        imageSaved.withOutputStream { os -> img.save(os, 'png') }
     }
 }
+
+def saved = ImageIO.read(imageSaved.toFile())
+new SwingBuilder().edt {
+    frame(title: "$resultCount detected objects", size: [w, h],
+            show: true, defaultCloseOperation: DISPOSE_ON_CLOSE) {
+        label(icon: imageIcon(image: saved))
+    }
+}
+
 /*
-car 0.996 [xmin:439, ymin:62, xmax:715, ymax:173]
-bicycle 0.986 [xmin:144, ymin:128, xmax:547, ymax:453]
-dog 0.919 [xmin:102, ymin:184, xmax:344, ymax:541]
+class: "car", probability: 0.99991, bounds: [x=0.611, y=0.137, width=0.293, height=0.160]
+class: "bicycle", probability: 0.95385, bounds: [x=0.162, y=0.207, width=0.594, height=0.588]
+class: "dog", probability: 0.93752, bounds: [x=0.168, y=0.350, width=0.274, height=0.593]
 */

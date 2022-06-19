2f181b685d64ba9fab1b2051c2175cbd4ca23213
Paul King <paulk@asert.com.au>
2022-06-19 16:49:21 +1000

    formatting

--- subprojects/WhiskeyWayang/src/main/groovy/WhiskeyWayang.groovy
+++ subprojects/WhiskeyWayang/src/main/groovy/WhiskeyWayang.groovy
@@ -80,12 +80,12 @@ int iterations = 20
 
 // read in data from our file
 def url = WhiskeyWayang.classLoader.getResource('whiskey.csv').file
-def pointsData = new File(url).readLines()[1..-1].collect { Point.fromLine(it) }
+def pointsData = new File(url).readLines()[1..-1].collect{ Point.fromLine(it) }
 def dims = pointsData[0].pts().size()
 
 // create some random points as initial centroids
 def r = new Random()
-def initPts = (1..k).collect { (0..<dims).collect(row -> r.nextGaussian() + 2) as double[] }
+def initPts = (1..k).collect { (0..<dims).collect {  r.nextGaussian() + 2 } as double[] }
 
 // create planbuilder with Java and Spark enabled
 def configuration = new Configuration()
@@ -98,7 +98,7 @@ def points = planBuilder
     .loadCollection(pointsData).withName('Load points')
 
 def initialCentroids = planBuilder
-    .loadCollection((0..<k).collect { idx -> new TaggedPointCounter(initPts[idx], idx, 0) })
+    .loadCollection((0..<k).collect{ idx -> new TaggedPointCounter(initPts[idx], idx, 0) })
     .withName("Load random centroids")
 
 def finalCentroids = initialCentroids
@@ -111,5 +111,5 @@ def finalCentroids = initialCentroids
 
 println 'Centroids:'
 finalCentroids.each {
-    println "Cluster$it.cluster: ${it.pts.collect { d -> sprintf('%.3f', d) }.join(', ')}"
+    println "Cluster$it.cluster: ${it.pts.collect{ d -> sprintf('%.3f', d) }.join(', ')}"
 }

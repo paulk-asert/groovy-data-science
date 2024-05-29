93940ad096e175236dfd509c96e4f560ca3fca50
Paul King <paulk@asert.com.au>
2024-05-29 17:37:24 +1000

    remove unused import

--- subprojects/WhiskeyBeam/src/main/groovy/WhiskeyBeamMeta.groovy
+++ subprojects/WhiskeyBeam/src/main/groovy/WhiskeyBeamMeta.groovy
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import groovy.transform.CompileStatic
 import org.apache.beam.sdk.Pipeline
 import org.apache.beam.sdk.transforms.Combine
 import org.apache.beam.sdk.transforms.Create
